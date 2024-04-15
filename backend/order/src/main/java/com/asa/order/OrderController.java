e09edbb47818614aab8d3e2366d838edf7d6e495
Aditya Chandran <adityachandrn1@gmail.com>
2024-04-15 07:25:05 +0530

    orders page

--- backend/order/src/main/java/com/asa/order/OrderController.java
+++ backend/order/src/main/java/com/asa/order/OrderController.java
@@ -4,9 +4,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.asa.order.models.Order;
@@ -18,8 +20,8 @@ public class OrderController {
   @Autowired
   private OrderService orderService;
 
-  @PostMapping("/get")
-  public ResponseEntity<Object> getOrder(@RequestBody String orderId) {
+  @GetMapping("/get")
+  public ResponseEntity<Object> getOrder(@RequestParam String orderId) {
     Object response = orderService.getOrder(orderId);
     return ResponseEntity.ok(response);
   }
